80e9696f536aa7b13b632fa81661764aa5c219f3
Rumi Chowdhury <cerebro680@gmail.com>
2022-07-31 17:01:46 +0600

    Map class changes

--- fao_PROJECT/Program.cs
+++ fao_PROJECT/Program.cs
@@ -9,7 +9,7 @@ using CsvHelper.Configuration;     //Library for reading in CSV files
 using CsvHelper.Configuration.Attributes;    //Library for reading in CSV files
 
 
-namespace fao
+namespace fao_PROJECT
 
 {
     class program
@@ -17,7 +17,7 @@ namespace fao
         static void Main(String[] args)
         {
 
-            /* Reads in input from csv files according to director */
+            /* Reads in input from csv files according to directory */
             using (var datacasesReader = new StreamReader(@"F:\proj\fao_PROJECT\fao_PROJECT\soft_assignment\data_cases_1.csv")) //use directory as desired
 
 
@@ -25,14 +25,15 @@ namespace fao
                 using (var csvReader = new CsvReader(datacasesReader, CultureInfo.InvariantCulture))
                 {
                     csvReader.Context.RegisterClassMap<classmapping>();
-                    var records = csvReader.GetRecords<cases>().ToList();
+                    var records = csvReader.GetRecords<cases>();
                 }
                 using (var diseaselistReader = new StreamReader(@"F:\proj\fao_PROJECT\fao_PROJECT\soft_assignment\disease_list.csv")) //use directory as desired
 
                 {
                     using (var csvReader = new CsvReader(diseaselistReader, CultureInfo.InvariantCulture))
                     {
-                        var records = csvReader.GetRecords<dynamic>().ToList();
+                        csvReader.Context.RegisterClassMap<diseasemapping>();
+                        var records = csvReader.GetRecords<diseases>();
                     }
 
                 }
@@ -41,28 +42,29 @@ namespace fao
         }
 
     }
-    public class classmapping : ClassMap<cases> //With cases as base class
+}
+    public class classmapping : ClassMap <cases> //With cases as base class
     {
-        public classmapping  //Map class
-        {  
-        Map( => m.uuid).Name("uuid");
+    public classmapping()  //Map class
+    {
+        Map(m => m.uuid).Name("uuid");
         Map(m => m.datetime).Name("datetime");
         Map(m => m.species).Name("species");
-        Map(m => m.number_morbidity.Name("number_morbidity");
+        Map(m => m.number_morbidity).Name("number_morbidity");
         Map(m => m.disease_id).Name("disease_id");
         Map(m => m.number_mortality).Name("number_mortality");
         Map(m => m.total_number_cases).Name("total_number_cases");
         Map(m => m.location).Name("location");
+        }
     }
-}
 
 
 public class diseasemapping : ClassMap<diseases> //With diseases as base class
 {
-    public classmapping  //Map class
+    public diseasemapping()  //Map class
             {
             Map(m => m.ID).Name("ID");
-    Map(m => m.name).Name("name");
+            Map(m => m.name).Name("name");
 }
        
     }
@@ -92,6 +94,7 @@ public class diseasemapping : ClassMap<diseases> //With diseases as base class
         public string species { get; set; }
 
         public int number_morbidity { get; set; }
+        
 
         public int disease_id { get; set; }
 
@@ -115,3 +118,4 @@ public class diseasemapping : ClassMap<diseases> //With diseases as base class
 }
 
 
+    
